e555683af285b5ae67f2ea8726c2212eabdd9bd3
Tanishq Sonwane <tanishqsonwane32@gmail.com>
2025-08-09 22:56:28 +0530

    landing pang created

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Nav = () => {
   return (
-    <div className='h-10 py-10 bg-[#00000000] px-8 w-full flex items-center justify-between'>
-        <div className=' h-20 w-20 '>
-            <img className='h-full w-full' src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
-        </div>
-        <div className='text-white gap-4 flex'>
-        <button className='px-8 py-1.5 border-gray-400 rounded-md border'>a  </button>
-        <button className='px-6 py-2 rounded-lg bg-[#e50914] font-semibold '>Sign in</button>
-        </div>
-    </div>
-  )
-}
+    <div className="px-8 py-4 flex items-center justify-between">
+      {/* Logo */}
+      <div className="h-10">
+        <img
+          className="h-full"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt="Netflix Logo"
+        />
+      </div>
 
-export default Nav
+      {/* Buttons */}
+      <div className="flex gap-4">
+        <button className="px-8 py-1.5 border border-gray-400 rounded-md text-white">
+          a
+        </button>
+        <button className="px-6 py-2 rounded-lg bg-[#e50914] font-semibold text-white">
+          Sign in
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import Nav from '../components/Nav'
+import React from "react";
+import Nav from "../components/Nav";
 
 const Main = () => {
   return (
-    <main className='relative h-screen w-full bg-black'>
-      <Nav/>
-      
-      <img 
-        className='h-1/2 w-full object-cover' 
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_small.jpg" 
-        alt="" 
+    <main className="relative h-screen w-full">
+      {/* Background Image */}
+      <img
+        className="absolute inset-0 h-full w-full object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_small.jpg"
+        alt="Netflix Background"
       />
 
-      <div className="absolute top-20 inset-0 bg-black opacity-80"></div>
-    </main>
-  )
-}
+      {/* Black Overlay */}
+      <div className="absolute inset-0 bg-black/80"></div>
 
-export default Main
+      {/* Nav on top */}
+      <div className="relative z-10">
+        <Nav />
+      </div>
+
+      {/* Center content (optional) */}
+      <div className="relative z-10 flex flex-col items-center justify-center text-white h-full">
+        <h1 className="text-5xl font-bold mb-4">Unlimited movies, TV shows and more</h1>
+        <p className="text-xl mb-6">Watch anywhere. Cancel anytime.</p>
+        <button className="px-6 py-3 bg-red-600 rounded-md font-semibold">
+          Get Started
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
