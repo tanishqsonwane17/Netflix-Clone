40bfef1dc35beea436fc5e2865826aba870c062f
Tanishq Sonwane <tanishqsonwane32@gmail.com>
2025-08-10 16:31:21 +0530

    nav fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="px-8 py-4 flex items-center justify-between">
+    <div className="px-8 py-6 flex items-center justify-between">
       {/* Logo */}
-      <div className="h-10">
+      <div className="h-8">
         <img
           className="h-full"
           src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
@@ -14,10 +14,10 @@ const Nav = () => {
 
       {/* Buttons */}
       <div className="flex gap-4">
-        <button className="px-8 py-1.5 border border-gray-400 rounded-md text-white">
+        <button className="px-6 py-1 border border-gray-400 rounded-md text-white">
           a
         </button>
-        <button className="px-6 py-2 rounded-lg bg-[#e50914] font-semibold text-white">
+        <button className="px-4 py-1 rounded-lg bg-[#e50914] font-semibold text-white">
           Sign in
         </button>
       </div>
--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -4,22 +4,14 @@ import Nav from "../components/Nav";
 const Main = () => {
   return (
     <main className="relative h-screen w-full">
-      {/* Background Image */}
       <img
         className="absolute inset-0 h-full w-full object-cover"
         src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_small.jpg"
-        alt="Netflix Background"
-      />
-
-      {/* Black Overlay */}
+        alt="Netflix Background"/>
       <div className="absolute inset-0 bg-black/80"></div>
-
-      {/* Nav on top */}
       <div className="relative z-10">
         <Nav />
       </div>
-
-      {/* Center content (optional) */}
       <div className="relative z-10 flex flex-col items-center justify-center text-white h-full">
         <h1 className="text-5xl font-bold mb-4">Unlimited movies, TV shows and more</h1>
         <p className="text-xl mb-6">Watch anywhere. Cancel anytime.</p>
